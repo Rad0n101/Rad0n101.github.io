dc462d3960ef72539ce8e0b37111af209c4e77f5
Radon <73435612+Rad0n101@users.noreply.github.com>
2022-09-22 04:09:46 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function ricroll(){
+    let r = "https://dropmefiles.com/5CzuC";
+    window.location.assign("https://dropmefiles.com/5CzuC");
+}
+
+function onButtonClick(){
+    let rnd = Math.floor(Math.random() * 3) + 1
+
+    switch (rnd){
+        case 1:
+            alert("Lorem ipsum dolor sit amet");
+            break;
+        case 2:
+            alert("https://dropmefiles.com/5CzuC");
+            break;
+        case 3:
+            alert("hello world?");
+            break;    
+    }
+}
