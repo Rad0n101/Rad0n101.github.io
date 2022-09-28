7b0b2e9c6ef84e8f08474a513f19e7ee61a4b185
Radon <73435612+Rad0n101@users.noreply.github.com>
2022-09-29 01:44:50 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-function ricroll(){
-    let r = "https://dropmefiles.com/5CzuC";
-    window.location.assign("https://dropmefiles.com/5CzuC");
-}
-
-function onButtonClick(){
-    let rnd = Math.floor(Math.random() * 3) + 1
-
-    switch (rnd){
-        case 1:
-            alert("Lorem ipsum dolor sit amet");
-            break;
-        case 2:
-            alert("Тут была ссылка на рикрол");
-            break;
-        case 3:
-            alert("hello world?");
-            break;    
-    }
+function onRegButtonClick(){
+    
 }
